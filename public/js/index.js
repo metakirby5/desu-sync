16eee1db543ec0a10e04ebbc5f33596e8d7edf9d
metakirby5 <echan@circleup.com>
2014-08-16 06:08:13 -0700

    major refactor of file upload... now it works i guess

--- public/js/index.js
+++ public/js/index.js
@@ -55,18 +55,101 @@
   }]).
 
   // Filenames and stuff
-  controller('entriesCtrl', ['$scope', 'pub', 'parser', 'hummingbird',
-  function($scope, pub, parser, hummingbird) {
+  controller('entriesCtrl', ['$scope', '$timeout', 'pub', 'parser', 'hummingbird',
+  function($scope, $timeout, pub, parser, hummingbird) {
     $scope.pub = pub.obj;
 
     $scope.entries = [];
     $scope.selectAll = false;
 
-    $scope.manualName = '';
+    $scope.manualTitle = '';
     $scope.manualEp = '';
 
     $scope.errors = [];
     $scope.ajaxing = false;
+
+    var added = [];
+    var queue = [];
+
+    // Basic functions
+
+    function addErr(data, msg, klass) {
+      data.err = msg;
+      if (klass) data.klass = klass;
+      $scope.errors.push(data);
+      $timeout(function() {
+        $scope.removeErr(data);
+      }, 5000);
+    }
+
+    $scope.removeErr = function(err) {
+      $scope.errors.splice($scope.errors.indexOf(err), 1);
+    };
+
+    // Will check for entries to kick before pushing.
+    function pushEntry(data) {
+      angular.forEach($scope.entries, function(entry) {
+        if (data.id === entry.id)
+          if (data.ep > entry.ep)
+            $scope.removeEntry(entry);
+      });
+      $scope.entries.push(data);
+    }
+
+    $scope.removeEntry = function(entry) {
+      $scope.entries.splice($scope.entries.indexOf(entry), 1);
+    };
+
+    $scope.countChecked = function() {
+      var count = 0;
+      angular.forEach($scope.entries, function(entry) {
+        if (entry.checked)
+          count++;
+      });
+      return count;
+    }
+
+    // Remove checked or unchecked
+    $scope.removeChecked = function(checked) {
+      angular.forEach($scope.entries.slice(), function(entry) {
+        if (entry.checked === checked)
+          $scope.removeEntry(entry);
+      });
+    };
+
+    $scope.removeAll = function() {
+      $scope.entries = [];
+    };
+
+    function enqueue(title, ep) {
+      queue.push({title: title, ep: ep});
+    }
+
+    function processQueued() {
+      angular.forEach(queue, function(entry) {
+        searchAndPush(entry.title, entry.ep);
+      });
+      queue = [];
+    }
+
+    // Adds to the 'added' array
+    function recordAdded(title, ep) {
+      added.push({title: title, ep: ep});
+    }
+
+    function shouldAdd(title, ep) {
+      var retval = true;
+      angular.forEach(added.concat(queue), function(entry) {
+        if (title === entry.title) {
+          if (ep <= entry.ep)
+            retval = false;
+          else { // Clean up smaller entries
+            queue.splice(entry, 1);
+          }
+        }
+      });
+      return retval;
+    }
 
     /*
     Callback takes err, data:
@@ -80,136 +163,86 @@
     */
     function search(title, ep, callback) {
       $scope.ajaxing = true;
+      var errObj = {title: title, ep: ep};
       hummingbird.search(parser.querify(title)).
         success(function(data) {
           console.log(data);
           if(!(data && data[0])) {
-            callback('Anime not found');
+            callback('Anime not found', errObj);
             return;
           }
 
           var entry = data[0];
           console.log(entry);
-          callback(null, {
-            id: entry.id,
-            title: entry.title,
-            ep: ep,
-            max: entry.episode_count,
-            img: entry.cover_image,
-            checked: true
-          });
+          if (ep <= entry.episode_count) {
+            callback(null, {
+              id: entry.id,
+              title: entry.title,
+              ep: ep,
+              img: entry.cover_image,
+              checked: true
+            });
+          }
+          else {
+            callback(entry.title + ' only has ' + entry.episode_count + ' episodes', errObj);
+          }
           $scope.ajaxing = false;
         }).
         error(function(data) {
           console.log(data);
-          callback('AJAX error');
+          callback('AJAX error', errObj);
           $scope.ajaxing = false;
         });
     }
 
-    function pushEntry(data) {
-      $scope.entries.push(data);
-    }
-
-    function shouldAdd(title, ep) {
-      angular.forEach($scope.entries, function(entry) {
-        if (data.id === entry.id || data.title === entry.title) {
-          if (data.ep <= entry.ep) // Beat by other entry
-            return false;
+    // Returns success or failure
+    function searchAndPush(title, ep) {
+      search(title, ep, function(err, data) {
+        if (err) {
+          addErr(data, err, 'alert-danger');
+          return false;
         }
-      });
-      return true;
-    }
-
-    function insertEntry(data) {
-      var kicked = false;
-
-      angular.forEach($scope.entries, function(entry) {
-        if (data.id === entry.id || data.title === entry.title) {
-          if (data.ep > entry.ep) // Kick lower entries out
-            $scope.removeEntry(entry);
-          else {                  // Other entry beat us
-            kicked = true;
-            return;
-          }
-        }
-      });
-
-      if (!kicked)
         pushEntry(data);
-    }
-
-    function addEntry(title, ep, queriable) {
-      if (!shouldAdd(title, ep))
-        return;
-      if (queriable) {
-        search(title, ep, function(err, data) {
-          if (err) {
-            console.log(err);
-            addErr({title: title, ep: ep, err: err, klass: 'alert-danger'});
-            return;
-          }
-          insertEntry(data);
-        })
-      } else {
-        insertEntry({
-          title: title,
-          ep: ep ? ep : 1,
-          checked: false
-        });
-      }
-    }
-
-    function addErr(err) {
-      $scope.errors.push(err);
-    }
-
-    $scope.removeEntry = function(entry) {
-      $scope.entries.splice($scope.entries.indexOf(entry), 1);
-    };
-
-    $scope.removeErr = function(err) {
-      $scope.errors.splice($scope.errors.indexOf(err), 1);
-    };
-
-    $scope.count = function() {
-      var count = 0;
-      angular.forEach($scope.entries, function(entry) {
-        if (entry.checked)
-          count++;
+        return true;
       });
-      return count;
     }
 
-    $scope.addFiles = function(files) {
-      angular.forEach(files, function(file) {
-        var valid = !(file.type && file.type.indexOf('video'));
-        if (valid) {
-          var parsed = parser.parse(file.name);
-          addEntry(parsed.title, parsed.ep, true);
-        }
-        else
-          addEntry(file.name, 1);
-      });
-    };
-
-    // Manual text fields
+    // Manual text fields - no queuing needed
     $scope.manualAdd = function() {
-      addEntry($scope.manualName, $scope.manualEp, true);
-      $scope.manualName = '';
+      (function(title, ep) {
+        if (shouldAdd(title, ep)) {
+          recordAdded(title, ep);
+          searchAndPush(title, ep);
+        }
+      })($scope.manualTitle, $scope.manualEp);
+
+      $scope.manualTitle = '';
       $scope.manualEp = '';
     };
 
-    // Remove checked or unchecked
-    $scope.removeChecked = function(checked) {
-      angular.forEach($scope.entries.slice(), function(entry) {
-        if (entry.checked === checked)
-          $scope.removeEntry(entry);
-      });
-    };
+    function fileValid(file) {
+      return !(file.type && file.type.indexOf('video'));
+    }
 
-    $scope.removeAll = function() {
-      $scope.entries = [];
+    $scope.addFiles = function(files) {
+      // Queue so we don't make n queries
+      angular.forEach(files, function(file) {
+        if (!fileValid(file)) {
+          addErr({title: file.name, ep: 'n/a'}, 'File type not valid', 'alert-warning')
+          return;
+        }
+
+        var parsed = parser.parse(file.name);
+        var title = parsed.title;
+        var ep = parsed.ep;
+
+        if (shouldAdd(title, ep)) {
+          recordAdded(title, ep);
+          enqueue(title, ep);
+        }
+      });
+
+      processQueued();
     };
 
     $scope.$watch('selectAll', function(selected) {
